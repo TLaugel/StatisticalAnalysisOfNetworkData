cbe15af816d2683c7190d4a2955d36f431231e31
TLaugel <Thibault.Laugel@ensae.fr>
2014-12-26 14:14:52 +0100

    Update model2_covarianceMat.py

--- model2_covarianceMat.py
+++ model2_covarianceMat.py
@@ -15,54 +15,59 @@ else :
   maxDate = time.strptime("2002-12-31", "%Y-%m-%d")
 maxDateStr = '-'.join([str(maxDate.tm_year),str(maxDate.tm_mon),str(maxDate.tm_mday)]) 
 
+maxDate = "2002-12-31"
 
-#input is the output dataframe of the script model.py 
+
+
+
+###functions
+
+#rbarSerie : for a user, creates the matrix of size nbMovies^2 with M[i,j]=weight_user*rbar_user[i]*rbar_user[j]
+def rbarSerie(user):
+    M = df[["movieID", "centeredRating"]][df["userID"] == user] #je prends films id et rating centered pour mon user
+    M = pandas.merge(moviesall,M, left_on='movieID', right_on='movieID', how='left') #left join on list of unique movies
+    M = M["centeredRating"].fillna(0) #replace NaN from the left join by zeros and keep only centeredRating : this is now a Serie of size nbMovies
+    M = numpy.asmatrix(M.as_matrix()) #convert to an array then a matrix to get the transpose(not working for a 1 dimension vector with array format)
+    Mat = weight[user] * M.T * M
+    return M
+
+def onezeromat(matrix):
+    return numpy.asmatrix(numpy.where(matrix>0, 1, 0)) 
+
+####input is the output dataframe of the script model.py 
 fin = 'C:/Users/Thibault/Desktop/ENSAE/Cours3A/Network Data/download/dbEffects2002-12-31.txt'
 df = pandas.read_csv(fin,sep="\t",encoding="utf8")
 
+
+####Parameters
 nbMovies = len(pandas.Series(df["movieID"].values.ravel()).unique())
 UCnt = df.groupby('userID').agg(['count'])["rating"] #how many movies each user
 UCnt = UCnt.ix[:,0]
-
-
-
-#####Covariance Matrix : not finished
 weight = 1.0/UCnt #vector containing one weight for each user
+moviesall = df.groupby('movieID')[['movieID']].max()
 
-#intialize matrices
+###Matrices and Noise matrices initialization
 Cov = numpy.asmatrix( numpy.zeros((nbMovies, nbMovies))) 
 Wgt = Cov
-
-#generate noise matrix
 NoiseCov = numpy.asmatrix( numpy.zeros((nbMovies, nbMovies)))  #noise matrix
 NoiseWgt = NoiseCov
 
-#creer dataframe de tous les films
-#dans boucle, on va joindre rbarserie à ce df sur les films en left join > Null à r
-moviesall = df.groupby('movieID')[['movieID']].max()
 
-
-#vector rbar for each user = centeredreco for each movie, for 
+###loop on users: for each user, we compute its corresponding Cov and Wgt matrices and aggregate them to the others
 for user in pandas.Series(df["userID"].values.ravel()).unique():
-    rbarSerie = df[["movieID", "centeredRating"]][df["userID"] == user] #je prends films id et rating centered pour mon user
-    rbarSerie = pandas.merge(moviesall,rbarSerie, left_on='movieID', right_on='movieID', how='left') #left join on list of unique movies
-    rbarSerie = rbarSerie["centeredRating"].fillna(0) #replace NaN from the left join by zeros and keep only centeredRating : this is now a Serie of size nbMovies
-    rbarSerie = numpy.asmatrix(rbarSerie.as_matrix()) #convert to an array then a matrix to get the transpose(not working for a 1 dimension vector with array format)
-    eu = numpy.where(rbarSerie>0, 1, 0) #should be an array
-    eu = numpy.asmatrix(eu)
-    rbarMatrix = weight[user] * rbarSerie.T * rbarSerie # matrix size nbMovies^2
-    euMatrix = weight[user] * eu.T * eu #idem
+    rbarMatrix = rbarSerie(user) #matrix of size nbMovies^2 #weight[user] * rbar.T * rbar # matrix size nbMovies^2
+    euMatrix = onezeromat(rbarMatrix) #idem with 1 instead of the ratings #weight[user] * eu.T * eu #idem
     Cov = Cov + rbarMatrix #let's sum baby
     Wgt = Wgt + euMatrix
+
 Cov = Cov + NoiseCov
 Wgt = Wgt + NoiseWgt
 
+
+###Ouput files : we save each matrix in a separate txt file
 path = 'C:/Users/Thibault/Desktop/ENSAE/Cours3A/Network Data/download/'
 foutCov = path+'CovMatrix'+maxDate+'.txt'
 foutWgt = path+'WgtMatrix'+maxDate+'.txt'
 
 numpy.savetxt(foutCov,Cov,delimiter=',')
 numpy.savetxt(foutWgt,Wgt,delimiter=',')
-
-###########################
-# NB benjamin : j'ai lancé, ca avait l'air de tourner mais pas encore (26/12) vérifié que les matrice en sortie était juste!
