206014b5c17f9e3f5ca0daa3218c63c3bcc51001
B. DONNOT ubuntu <you@example.com>
2014-12-28 00:55:22 +0100

    another memory improvment

--- model2_covarianceMat.py
+++ model2_covarianceMat.py
@@ -22,6 +22,9 @@ if sys.platform == 'linux2':
 	path = '../'
 fin = path+'dbEffects'+maxDate+'.txt'
 df = pandas.read_csv(fin,sep="\t",encoding="utf8")
+VarKept = ['movieID','userID','centeredRating']
+VarDel =  [el for el in df if not el in VarKept]
+df.drop(VarDel, axis=1, inplace=True)
 
 ###functions
 from scipy.sparse import csc_matrix as sparseM
@@ -46,11 +49,13 @@ def rbarSerie(user):
 def onezeromat(matrix):
     return numpy.asmatrix(numpy.where(matrix>0, 1, 0)) 
 
-
+#~ def onezeroAux(x):
+	#~ return int(x>0)
+#~ oneZero= numpy.vectorize(onezeroAux, otypes=[numpy.int])
 
 ####Parameters
 nbMovies = len(pandas.Series(df["movieID"].values.ravel()).unique())
-UCnt = df.groupby('userID').agg(['count'])["rating"] #how many movies each user
+UCnt = df.groupby('userID').agg(['count'])["movieID"] #how many movies each user
 UCnt = UCnt.ix[:,0]
 weight = 1.0/UCnt #vector containing one weight for each user
 moviesall = df.groupby('movieID')[['movieID']].max()
@@ -58,11 +63,6 @@ moviesall = df.groupby('movieID')[['movieID']].max()
 ###Matrices and Noise matrices initialization
 Cov = numpy.asmatrix( numpy.zeros((nbMovies, nbMovies))) 
 Wgt = Cov.copy()
-
-
-M = df[["movieID", "centeredRating"]].copy()
-M = pandas.merge(moviesall,M, left_on='movieID', right_on='movieID', how='left') 
-
 
 print "I am here"
 ###loop on users: for each user, we compute its corresponding Cov and Wgt matrices and aggregate them to the others
@@ -73,27 +73,31 @@ timestart =  time.time()
 df.set_index(['userID']) #small improvment : a bit faster...
 for user in pandas.Series(df["userID"].values.ravel()).unique():
 	rbarMatrix = rbarSerie(user) #matrix of size nbMovies^2 #weight[user] * rbar.T * rbar # matrix size nbMovies^2
-	euMatrix = onezeromat(rbarMatrix.todense()) #idem with 1 instead of the ratings #weight[user] * eu.T * eu #idem
+	#~ euMatrix = onezeromat(rbarMatrix.todense()) #idem with 1 instead of the ratings #weight[user] * eu.T * eu #idem
 	Cov += rbarMatrix.todense() #let's sum baby
-	Wgt = Wgt + euMatrix
-	#~ print "tut"
-	
-print  time.time()-timestart
-NoiseCov = numpy.asmatrix( numpy.zeros((nbMovies, nbMovies)))  #noise matrix
-NoiseWgt = NoiseCov.copy()
-Cov = Cov + NoiseCov
-Wgt = Wgt + NoiseWgt
+	Wgt += numpy.max(rbarMatrix.todense(),0)
+	#~ if i > 100 :
+		#~ break
+	#~ i += 1
+print  time.time()-timestart 
 
 
-###Cleaning the covariance matrix
-beta = 0  # NB :  the paper states that they used different values of beta for the diagonal and for the rest> to consider
-Cov = Cov + beta * Cov.mean()
-Wgt = Wgt + beta * Wgt.mean()
-Cov = numpy.divide(Cov,Wgt) #division term by term
+##No Noise for now ... (Memory issue)
+#~ NoiseCov = numpy.asmatrix( numpy.zeros((nbMovies, nbMovies)))  #noise matrix
+#~ NoiseWgt = NoiseCov.copy()
+#~ Cov += NoiseCov
+#~ Wgt += Wgt NoiseWgt
+
+
+#~ ###Cleaning the covariance matrix
+#~ beta = 0  # NB :  the paper states that they used different values of beta for the diagonal and for the rest> to consider
+#~ Cov += beta * Cov.mean()
+#~ Wgt += beta * Wgt.mean()
+#~ Cov = numpy.divide(Cov,Wgt) #division term by term
 
 ###Ouput files : we save each matrix in a separate txt file
 #~ path = 'C:/Users/Thibault/Desktop/ENSAE/Cours3A/Network Data/download/'
-foutCov = path+'CovMatrix'+maxDate+'.txt'
-#foutWgt = path+'WgtMatrix'+maxDate+'.txt'
+foutCov = path+'CovMatrix_'+maxDate+'.txt'
+foutWgt = path+'WgtMatrix_'+maxDate+'.txt'
 numpy.savetxt(foutCov,Cov,delimiter=',')
-#numpy.savetxt(foutWgt,Wgt,delimiter=',')
+numpy.savetxt(foutWgt,Wgt,delimiter=',')
